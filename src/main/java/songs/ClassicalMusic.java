bd1a5c080b8d4cf8160489cbebabb2f181a730ca
Țurcanu Andrei <andrei.turcanu@unifun.com>
2019-12-22 01:09:29 +0200

    using facotry method to create object in class with method , not in main using new keyword , using spring for this

--- src/main/java/songs/ClassicalMusic.java
+++ src/main/java/songs/ClassicalMusic.java
@@ -2,7 +2,22 @@ package songs;
 
 public class ClassicalMusic implements Music {
 
+    //private constructor for factory method
+    private ClassicalMusic(){};
+    //factory method
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("DOing my initializaation ");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     public String getSong() {
         return "Vivaldi - FourSeason";
     }
+
 }
--- src/main/java/songs/TestSpring.java
+++ src/main/java/songs/TestSpring.java
@@ -6,24 +6,29 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("aplicationContext.xml");
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean1",ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+
+
 //       Music music =  context.getBean("musicBean",Music.class);
 //       MusicPlayer musicPlayer = new MusicPlayer(music);
 
-
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer",MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer",MusicPlayer.class);
-
-
-        System.out.println(musicPlayer1);
-        musicPlayer1.setVolume(90);
-        System.out.println(musicPlayer2);
-        musicPlayer1.playMusicList();
-
-        System.out.println(musicPlayer1.getName());
-        System.out.println(musicPlayer1.getVolume());
-
-        System.out.println(musicPlayer2.getName());
-        System.out.println(musicPlayer2.getVolume());
+//
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer",MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer",MusicPlayer.class);
+//
+//
+//        System.out.println(musicPlayer1);
+//        musicPlayer1.setVolume(90);
+//        System.out.println(musicPlayer2);
+//        musicPlayer1.playMusicList();
+//
+//        System.out.println(musicPlayer1.getName());
+//        System.out.println(musicPlayer1.getVolume());
+//
+//        System.out.println(musicPlayer2.getName());
+//        System.out.println(musicPlayer2.getVolume());
 
         context.close();
     }
