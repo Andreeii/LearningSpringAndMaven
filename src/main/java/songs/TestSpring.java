fbf36a0b48fad124895f98321d17115c4c9ef795
Andrei Turcanu <andriesturcanu@gmail.com>
2019-12-24 17:25:58 +0200

    Dependency injections via @Anotations, here we have two dependency , computer from MusicPlayer, MusicPlayer from Classical or Rock Music, this dependency is injected with @Autowired annotation

--- src/main/java/songs/TestSpring.java
+++ src/main/java/songs/TestSpring.java
@@ -2,19 +2,27 @@ package songs;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.awt.*;
+
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("aplicationContext.xml");
 
-        Music music = context.getBean("rockMusic",Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("rockMusic",Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//
+//        Music music1 = context.getBean("classicalMusic",Music.class);
+//        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+//
+//        musicPlayer1.playMusic();
+//        musicPlayer.playMusic();
 
+//        MusicPlayer musicPlayer =context.getBean("musicPlayer",MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        Music music1 = context.getBean("classicalMusic",Music.class);
-        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
-
-        musicPlayer1.playMusic();
-        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer",Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
